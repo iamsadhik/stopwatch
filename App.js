c7e7713361fa1ba21ec954c763dd479f6ca8c065
iamsadhik <ms0302500@gmail.com>
2021-05-27 16:58:55 +0530

    feat:minutes

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 window.onload =function() {
     var seconds = 00;
     var tens = 00;
+    var minutes= 00;
     var appendTens = document.getElementById("tens");
     var appendSeconds = document.getElementById("seconds");
+    var appendMinutes = document.getElementById("minutes")
     var buttonStart = document.getElementById("start");
     var buttonStop = document.getElementById("stop");
     var buttonReset = document.getElementById("reset");
@@ -40,11 +42,22 @@ window.onload =function() {
                 seconds++;
                 appendSeconds.innerHTML = "0" + seconds;
                 tens = 0;
-                appendTens.innerHTML ="0" + 0;
+                appendTens.innerHTML ="0" + tens;
             }
 
             if(seconds > 9){
                 appendSeconds.innerHTML = seconds;
+            }
+
+            if(seconds > 59){
+                seconds=0;
+                appendSeconds.innerHTML = "0" + seconds;
+                minutes++;
+                appendMinutes.innerHTML = "0" + minutes;
+                }
+
+            if(minutes > 9 ){
+                appendMinutes.innerHTML = minutes;
             }
         }
     
